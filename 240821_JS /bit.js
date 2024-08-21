320c9f81cc6d07abca9a5529795056e66d95e7e9
Kyoungeun Jo <kavvy99@naver.com>
2024-08-21 17:17:48 +0900

    24/08/21 JS bit operator practice

--- 240821_JS /bit.js
+++ 240821_JS /bit.js
@@ -1,4 +1,4 @@
-// bit 연산자
+```bit 연산자```;
 
 let a = 0b1010;
 let b = 0b1100;
@@ -21,3 +21,25 @@ console.log(a >> 1); // 5 (101)
 console.log(a << 1); // 20 (10100)
 console.log(b >> 1); // 6 (110)
 console.log(b << 1); // 24 (11000)
+
+`practice`;
+const R = 1; // 0b001
+const W = 2; // 0b010
+const E = 4; // 0b100
+
+let auth = parseInt("011", 2); // 2진수인 "011"을 10진수로 정수화하라
+console.log("🚀 ~ auth:", auth); // 3
+console.log("🚀 ~ auth(2):", `0b${auth.toString(2)}`); // 다시 2진수로
+
+const hasWriteAuth = auth & W; // 011 & 010
+console.log("🚀 ~ hasWriteAuth:", hasWriteAuth); // 010 (2)
+
+const hasExeAuth = !!(auth & E); // !!(011 & 100) // !!(000)
+console.log("🚀 ~ hasExeAuth:", hasExeAuth); // False
+
+const hasReadAndExeAuth = !!(auth & (R | E)); // !!(011 & (001 | 100)) === !!(011 & 101) === !!001
+console.log("🚀 ~ hasReadAndExeAuth:", hasReadAndExeAuth); // True
+
+// XOR: 같으면 0, 다르면 1
+auth = auth ^ E; // 011 ^ 100
+console.log("🚀 ~ auth:", auth); // 111 (7)
