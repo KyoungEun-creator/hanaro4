f8f6fe0e278060b7b3bea875f4d8c6353104d7e9
Kyoungeun Jo <kavvy99@naver.com>
2024-08-21 17:22:00 +0900

    24/08/21 JS array

---
+++ 240821_JS /array.js
@@ -0,0 +1,9 @@
+arr = [1, 2, 3];
+arr.hasOwnProperty("length"); // true
+
+const weekNames = "일월화수목금토";
+console.log("🚀 weekNames:", [...weekNames]); // ['일', '월', '화', '수', '목', '금', '토']
+
+user = { id: 1, name: "Lucy" };
+delete user.name;
+console.log(user); // { id: 1 }
