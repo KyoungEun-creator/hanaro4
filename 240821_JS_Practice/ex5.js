37f978c9a6e80ed6c691e10da39c61da2a6b701a
Kyoungeun Jo <kavvy99@naver.com>
2024-08-21 22:09:07 +0900

    JS practice5 again

--- 240821_JS_Practice/ex5.js
+++ 240821_JS_Practice/ex5.js
@@ -1,6 +1,7 @@
 // 추가문제) 주어진 값의 sqrt 값을 출력하고,
 // 1 큰 수가 무리수가 아닐 때 까지 sqrt 값을 출력하는 함수를 작성하시오.
 
+`방법1`;
 function printIrr(i) {
   let x = i;
   console.log(x, Math.sqrt(x).toFixed(3));
@@ -10,5 +11,19 @@ function printIrr(i) {
   }
 }
 
+printIrr(5);
+printIrr(9);
+
+// --------------------
+
+`방법2`;
+function printIrr(n) {
+  do {
+    const s = Math.sqrt(n);
+    console.log(n, +s.toFixed(3));
+    if (Math.sqrt(n + 1) % 1 === 0) break;
+    n += 1;
+  } while (true);
+}
 printIrr(5);
 printIrr(9);
