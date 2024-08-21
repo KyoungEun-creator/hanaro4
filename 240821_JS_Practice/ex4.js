b420787125c2acda2fda8a129ebb60ac401f8bae
Kyoungeun Jo <kavvy99@naver.com>
2024-08-21 21:52:31 +0900

    JS practice4 again again

--- 240821_JS_Practice/ex4.js
+++ 240821_JS_Practice/ex4.js
@@ -2,22 +2,26 @@
 // (단, 소숫점 자리수는 긴쪽에 맞춘다)
 
 `방법1`;
-function addPoints1(a, b) {
-  const maxLen = Math.max(a.toString().length, b.toString().length) - 2;
-  const sumVal = a + b;
-  console.log(sumVal.toFixed(maxLen));
-}
+// function addPoints1(a, b) {
+//   const maxLen = Math.max(a.toString().length, b.toString().length) - 2;
+//   const sumVal = a + b;
+//   console.log(sumVal.toFixed(maxLen));
+// }
 
-addPoints1(0.21354, 0.1);
-addPoints1(0.14, 0.28);
-addPoints1(0.34, 0.226);
+// addPoints1(0.21354, 0.1);
+// addPoints1(0.14, 0.28);
+// addPoints1(0.34, 0.226);
 
 `방법2`;
 // 정수부분이 무조건 0이 아니라면?
 
+function getLength(n) {
+  return String(n).split(".")[1].length;
+}
+
 function addPoints2(a, b) {
-  const aLen = +String(a).split(".")[1].length;
-  const bLen = +String(b).split(".")[1].length;
+  const aLen = getLength(a);
+  const bLen = getLength(b);
   const maxLen = Math.max(aLen, bLen);
   const sumVal = a + b;
   console.log(sumVal.toFixed(maxLen));
