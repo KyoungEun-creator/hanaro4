78b4c8430c03d2404484b5fba3100dc1e9e3bb33
Kyoungeun Jo <kavvy99@naver.com>
2024-08-23 00:25:17 +0900

    JS destructuring - practice2

---
+++ 240822_JS_Practice/ex2.js
@@ -0,0 +1,9 @@
+// 다음 user 객체에서 passwd 프로퍼티를 제외한 데이터를 userInfo 라는 변수에 할당하시오.
+
+const user = { id: 1, name: "Hong", passwd: "xxx", addr: "Seoul" };
+
+let { passwd, ...rest } = user;
+const userInfo = rest;
+console.log("🚀   userInfo:", userInfo);
+
+// 출력결과: {id: 1, name: 'Hong', addr: 'Seoul'}
