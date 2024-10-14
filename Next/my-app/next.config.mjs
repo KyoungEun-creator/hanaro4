0cd13c90c9c27bb962a1881e24637f2d671aa659
Kyoungeun Jo <kavvy99@naver.com>
2024-10-14 14:11:28 +0900

    241010 - auto remove console when build

--- Next/my-app/next.config.mjs
+++ Next/my-app/next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  compiler: {
+    // build 시 소스코드의 console.log 자동제거
+    removeConsole: {
+      exclude: ['error'],
+    },
+  },
+};
 
 export default nextConfig;
