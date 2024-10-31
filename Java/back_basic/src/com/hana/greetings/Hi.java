8110e64d452071311f7103a64e1c815f214dd26b
Kyoungeun Jo <kavvy99@naver.com>
2024-10-31 15:40:29 +0900

    switch case

--- Java/back_basic/src/com/hana/greetings/Hi.java
+++ Java/back_basic/src/com/hana/greetings/Hi.java
@@ -3,5 +3,20 @@ package com.hana.greetings;
 public class Hi {
 	public static void main(String[] args) {
 		System.out.println("Hi~! ");
+		int i = 0;
+		System.out.println("i = " + i);
+
+		playSwitch();
+	}
+
+	private static void playSwitch() {
+		int score = System.currentTimeMillis() % 2 == 0 ? 81 : 70;
+		final String grade = score > 80 ? "A" : "B";
+		switch (grade) {
+			case "A" -> System.out.println("Excellent");
+			case "B" -> System.out.println("Good");
+			case "C" -> System.out.println("Not bad");
+			default -> System.out.println("default");
+		}
 	}
 }
