1373f5aab8a8846cede049a32dd3245fc19aea1b
Kyoungeun Jo <kavvy99@naver.com>
2024-11-06 21:34:57 +0900

    놀이공원 범퍼카 탑승여부 boolean 값 출력하기

---
+++ Java/back_basic/src/trythis/bumperCar.java
@@ -0,0 +1,12 @@
+package trythis;
+
+import java.util.Scanner;
+
+public class bumperCar {
+	public static void main(String[] args) {
+		System.out.println("어린이의 신장(cm)을 입력하세요: ");
+		Scanner scan = new Scanner(System.in);
+		int height = scan.nextInt();
+		System.out.println(height >= 125);
+	}
+}
